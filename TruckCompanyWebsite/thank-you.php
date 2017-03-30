187ce62df0b5c9a3a2d5bf238c837bfe01d15150
Dustin Ledbetter <dustinledbetter9@gmail.com>
2017-03-30 01:56:42 -0400

    Add files via upload

---
+++ TruckCompanyWebsite/thank-you.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Truck Company Thanks You!</title>
+      <link rel="STYLESHEET" type="text/css" href="contact.css">
+</head>
+<body>
+
+<h2>Thanks for contacting us at Truck Company through our contact form page. 
+<br />
+<br />
+We try to check our emails as often as we can, but if we don't get back to you fast enough please call us at: (###)-###-####</h2>
+
+</body>
+</html>
